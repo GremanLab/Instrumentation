2c7a7cf71b67fb1569a2eb1fde43d45bfe53fdc4
Laboratoire Greman <baptiste.lefert@etu.univ-tours.fr>
2026-06-08 12:21:15 +0200

    Update Exemple oscilloscope.py

--- Oscilloscopes/Rhodes&Schwarz/Exemple oscilloscope.py
+++ Oscilloscopes/Rhodes&Schwarz/Exemple oscilloscope.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 # Initialisation de l'oscilloscope
 oscillo = RTM.RsInstrument('TCPIP::169.254.158.94::INSTR')
 
+# %% 
+
 # Regle certains parametres de l'oscilloscope
 oscillo.Calibers(channel=1, time_scale=5e-6)
 # 'channel' defini quel voies de l'oscilloscope est à modifier
@@ -24,11 +26,15 @@ t0, dt, data = oscillo.Measure(1)
 # dt : pas temporel entre chaques mesures
 # data : jeu de données
 
+# %% 
+
 vector_time = oscillo.Time_Vector(t0, dt, len(data))
 plt.plot(vector_time, data)
 plt.title("Diagramme de démonstration")
 plt.xlabel("Time (en s)")
 plt.ylabel("Amplitude (en V)")
 plt.show()
+
+# %% 
 
 oscillo.close()
